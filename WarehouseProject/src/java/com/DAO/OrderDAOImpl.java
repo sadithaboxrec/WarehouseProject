0752edd6c9a931ebb9ad24d393a249252349ddc6
Saditha Fernando <sadithafernandods2002@gmail.com>
2025-05-16 22:49:55 +0530

    Update for OrderDAO and implements

--- WarehouseProject/src/java/com/DAO/OrderDAOImpl.java
+++ WarehouseProject/src/java/com/DAO/OrderDAOImpl.java
@@ -4,10 +4,27 @@
  */
 package com.DAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.Entity.Order;
+
 /**
  *
  * @author sadit
  */
-public class OrderDAOImpl {
+public class OrderDAOImpl implements OrderDAO {
+    
+    	private Connection conn;
+
+	public OrderDAOImpl(Connection conn) {
+		super();
+		this.conn = conn;
+	}
+
     
 }
