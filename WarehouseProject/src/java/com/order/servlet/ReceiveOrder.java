26c9f997b4ad609a4bdbfb705aff2469ac7bbdd5
Saditha Fernando <sadithafernandods2002@gmail.com>
2025-05-16 23:06:32 +0530

    Update ReceiveOrder.java

--- WarehouseProject/src/java/com/order/servlet/ReceiveOrder.java
+++ WarehouseProject/src/java/com/order/servlet/ReceiveOrder.java
@@ -4,6 +4,11 @@
  */
 package com.order.servlet;
 
+import com.DAO.OrderDAOImpl;
+import com.DAO.OrderItemDAOImpl;
+import com.DAO.ProductDAOImpl;
+import com.DB.DBConnect;
+import com.Entity.OrderItem;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -11,6 +16,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
 
 /**
  *
@@ -19,6 +25,51 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/receive_order")
 public class ReceiveOrder extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    
+    public ReceiveOrder() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		int orderId = Integer.parseInt(request.getParameter("order_id"));
+
+        OrderItemDAOImpl itemDAO = new OrderItemDAOImpl(DBConnect.getConn());
+        ProductDAOImpl productDAO = new ProductDAOImpl(DBConnect.getConn());
+        OrderDAOImpl orderDAO = new OrderDAOImpl(DBConnect.getConn());
+
+        
+        List<OrderItem> items = itemDAO.getItemsByOrderId(orderId);
+
+        for (OrderItem item : items) {
+            int productId = item.getProductId();
+            int qty = item.getQuantity();
+
+            
+            productDAO.increaseStock(productId, qty);
+        }
+
+        
+                    boolean f = orderDAO.markAsReceived(orderId);
+
+            if (f) {
+                request.setAttribute("SuccessMsg", "Order Received");
+                request.getRequestDispatcher("/ViewOrderServlet").forward(request, response);
+            } else {
+                request.setAttribute("failedMsg", "Something went wrong on server");
+                request.getRequestDispatcher("/ViewOrderServlet").forward(request, response);
+            }
+       
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		
+	}
 
 
 }
