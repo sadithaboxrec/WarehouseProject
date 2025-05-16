fa23fed0d29576d0db3a011676c5b060c7a6670a
Saditha Fernando <sadithafernandods2002@gmail.com>
2025-05-16 22:59:16 +0530

    Update OrderItemDAOImpl.java

--- WarehouseProject/src/java/com/DAO/OrderItemDAOImpl.java
+++ WarehouseProject/src/java/com/DAO/OrderItemDAOImpl.java
@@ -4,10 +4,45 @@
  */
 package com.DAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 /**
  *
  * @author sadit
  */
 public class OrderItemDAOImpl {
+    
+    	
+	private Connection conn;
+
+	public OrderItemDAOImpl(Connection conn) {
+		super();
+		this.conn = conn;
+	}
+
+	
+	public boolean addOrderItem(int orderId, int productId, int quantity) {
+		
+		try {
+			
+			String sql = "INSERT INTO order_items (order_id, product_id, quantity) VALUES (?, ?, ?)";
+			
+			PreparedStatement ps=conn.prepareStatement(sql);
+			
+			ps.setInt(1, orderId);
+	        ps.setInt(2, productId);
+	        ps.setInt(3, quantity);
+                if(ps.executeUpdate()==1){
+                    return true;
+                }else{
+                    return false;
+                }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+                return false;
+	}
+	
     
 }
