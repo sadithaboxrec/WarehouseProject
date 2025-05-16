d22fa818f093e8f113fc4a9030abbcee4cf70b0d
Saditha Fernando <sadithafernandods2002@gmail.com>
2025-05-16 23:05:18 +0530

    Create ReceiveOrder.java

---
+++ WarehouseProject/src/java/com/order/servlet/ReceiveOrder.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.order.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author sadit
+ */
+
+@WebServlet("/receive_order")
+public class ReceiveOrder extends HttpServlet {
+
+
+}
