63b28dc72a68a936fce2009a2b0b17b59caeeacb
Saditha Fernando <sadithafernandods2002@gmail.com>
2025-05-16 22:56:53 +0530

    Create OrderItemDAO.java

---
+++ WarehouseProject/src/java/com/DAO/OrderItemDAO.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.DAO;
+
+import com.Entity.OrderItem;
+
+
+/**
+ *
+ * @author sadit
+ */
+public interface OrderItemDAO {
+    	
+	public boolean addOrderItem(int orderId, int productId, int quantity);
+	
+	public List<OrderItem> getItemsByOrderId(int orderId);
+}
