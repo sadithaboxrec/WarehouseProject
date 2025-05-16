ef6c38c258b08719fd6e436a03a4611401692e55
Saditha Fernando <sadithafernandods2002@gmail.com>
2025-05-16 23:00:25 +0530

    including addOrderItem in dao

--- WarehouseProject/src/java/com/DAO/OrderItemDAOImpl.java
+++ WarehouseProject/src/java/com/DAO/OrderItemDAOImpl.java
@@ -4,8 +4,12 @@
  */
 package com.DAO;
 
+import com.Entity.OrderItem;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -44,5 +48,37 @@ public class OrderItemDAOImpl {
                 return false;
 	}
 	
+        
+        	public List<OrderItem> getItemsByOrderId(int orderId) {
+		
+	    List<OrderItem> list = new ArrayList<>();
+	    
+	    
+	    
+	    try  {
+	    	
+	    	String sql = "SELECT * FROM order_items WHERE order_id = ?";
+	    	
+	    	PreparedStatement ps=conn.prepareStatement(sql);
+	    	
+	        ps.setInt(1, orderId);
+	        
+	        ResultSet rs = ps.executeQuery();
+	        
+	        while (rs.next()) {
+	        	
+	            OrderItem item = new OrderItem();
+	            item.setProductId(rs.getInt("product_id"));
+	            item.setQuantity(rs.getInt("quantity"));
+	            list.add(item);
+	            
+	        }
+	        
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    
+	    return list;
+	}
     
 }
